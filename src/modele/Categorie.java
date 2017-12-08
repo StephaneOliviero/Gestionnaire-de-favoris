a122c2612d6ef2e2c52dce5d305028c30f92797c
Stephane <Stephane@example.com>
2017-12-08 10:38:29 +0100

    Initialisation

---
+++ src/modele/Categorie.java
@@ -0,0 +1,9 @@
+package modele;
+
+public class Categorie {
+
+	private long idCategorie;
+	private String nomCategorie;
+	
+	
+}
---
+++ src/modele/Favoris.java
@@ -0,0 +1,9 @@
+package modele;
+
+public class Favoris {
+	
+	private long idFavoris;
+	private String nomFavoris;
+	private String urlFavoris;
+	private long idCategorie;//Un favori doit avoir au minimun une catégorie.
+}
